46f3dd37a6a9b74d1565fca5c7552481e3e6a4d4
Jonathan <jonang@ebay.com>
2019-04-28 22:33:20 -0700

    site revamp

--- js/agency.js
+++ js/agency.js
@@ -23,11 +23,33 @@
             $('.navbar-toggle:visible').click();
     });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: $(window).width()*.31
-        }
-    })
+    var toggleAffix = function(affixElement, scrollElement, wrapper) {
+
+      var height = affixElement.outerHeight(),
+          top = wrapper.offset().top;
+
+      if (scrollElement.scrollTop() >= top){
+          wrapper.height(height);
+          affixElement.addClass("affix");
+      }
+      else {
+          affixElement.removeClass("affix");
+          wrapper.height('auto');
+      }
+
+    };
+
+    $('[data-toggle="affix"]').each(function() {
+      var ele = $(this),
+          wrapper = $('<div></div>');
+
+      ele.before(wrapper);
+      $(window).on('scroll resize', function() {
+          toggleAffix(ele, $(this), wrapper);
+      });
+
+      // init
+      toggleAffix(ele, $(window), wrapper);
+    });
 
 })(jQuery); // End of use strict
